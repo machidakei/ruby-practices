725ecd22f46f68b614fded6d51133e2bf0f71727
machidakei <mochiduki.mochizuki@gmail.com>
2021-12-16 12:05:59 +0900

    リファクタリングを完了

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+# !/usr/bin/env ruby
 
 score = ARGV[0]
 scores = score.split(',')
@@ -34,20 +34,48 @@ frames.each_with_index do |frame, i|
              else
                frame.sum
              end
-           elsif i == 8
-             if frame[0] == 10
-               if frames[9][0] == 10
-                 10 + 10 + frames[9][2]
-               else
-                 10 + frames[9][0] + frames[9][1]
-               end
-             elsif frame.sum == 10
-               10 + frames[9][0]
-             else
-               frame.sum
-             end
-           else
-             frame.sum
-           end
+            end
+if frames[8][0] == 10
+  if frames[9][0] == 10
+    point += 10 + 10 + frames[9][2]
+  else
+    point += 10 + frames[9][0] + frames[9][1]
+  end
+elsif frames[8].sum == 10
+  point += 10 + frames[9][0]
+else
+  point += frames[8].sum
 end
+point += frames[9].sum
+
 puts point
+
+# frames.each_with_index do |frame, i|
+#   point += if i <= 7
+#              if frame[0] == 10
+#                if frames[i + 1][0] == 10
+#                  10 + 10 + frames[i + 2][0]
+#                else
+#                  10 + frames[i + 1].sum
+#                end
+#              elsif frame.sum == 10
+#                frames[i].sum + frames[i + 1][0]
+#              else
+#                frame.sum
+#              end
+#            elsif i == 8
+#              if frame[0] == 10
+#                if frames[9][0] == 10
+#                  10 + 10 + frames[9][2]
+#                else
+#                  10 + frames[9][0] + frames[9][1]
+#                end
+#              elsif frame.sum == 10
+#                10 + frames[9][0]
+#              else
+#                frame.sum
+#              end
+#            else
+#              frame.sum
+#            end
+# end
