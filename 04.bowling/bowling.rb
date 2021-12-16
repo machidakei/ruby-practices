126ef0260df4b87605e90506b2b123c3fb51bdd6
machidakei <mochiduki.mochizuki@gmail.com>
2021-12-16 12:06:58 +0900

    不要なコメントを削除

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -49,33 +49,3 @@ end
 point += frames[9].sum
 
 puts point
-
-# frames.each_with_index do |frame, i|
-#   point += if i <= 7
-#              if frame[0] == 10
-#                if frames[i + 1][0] == 10
-#                  10 + 10 + frames[i + 2][0]
-#                else
-#                  10 + frames[i + 1].sum
-#                end
-#              elsif frame.sum == 10
-#                frames[i].sum + frames[i + 1][0]
-#              else
-#                frame.sum
-#              end
-#            elsif i == 8
-#              if frame[0] == 10
-#                if frames[9][0] == 10
-#                  10 + 10 + frames[9][2]
-#                else
-#                  10 + frames[9][0] + frames[9][1]
-#                end
-#              elsif frame.sum == 10
-#                10 + frames[9][0]
-#              else
-#                frame.sum
-#              end
-#            else
-#              frame.sum
-#            end
-# end
