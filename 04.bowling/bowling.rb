9c865bd8bdb1568aad01c1a5da37506c16a8a33a
machidakei <mochiduki.mochizuki@gmail.com>
2021-12-22 13:33:15 +0900

    エラー解消

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -54,7 +54,7 @@ frames << frame_of_tenth_shot
 
 point = 0
 frames.each_with_index do |frame, i|
-  next if i < 9
+  next if i >= 9
   point += if frame[0] == 10
              if frames[i + 1][0] == 10
                if i == 8
@@ -76,5 +76,4 @@ frames.each_with_index do |frame, i|
            end
 end
 point += frames[9].sum
-
 puts point
