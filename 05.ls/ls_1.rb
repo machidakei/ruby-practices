7e08a970b38b6ca06bd4b557e6bcbc7c0452903b
machidakei <mochiduki.mochizuki@gmail.com>
2022-01-04 11:14:42 +0900

    レビュー内容から修正

--- 05.ls/ls_1.rb
+++ 05.ls/ls_1.rb
@@ -1,33 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-class Table
-  def print_table(elements_of_table)
-    length = elements_of_table.flatten.compact.max_by(&:length).length
-    elements_of_table.each do |row|
-      row.each do |item|
-        print item.to_s.ljust(length), "\t"
-      end
-      puts ''
+def print_table(elements_of_table)
+  length = elements_of_table.flatten.compact.max_by(&:length).length
+  elements_of_table.each do |row|
+    row.each do |item|
+      print item.to_s.ljust(length), "\t"
     end
+    puts ''
   end
 end
 
 array = Dir.glob('*')
 
 row_length = array.length / 3
-if array >= 4
-  horizontal_array = array.sort.each_slice(row_length + 1).map { |arr| arr }
-else
-  horizontal_array = array.sort.each_slice(1).map { |arr| arr }
-end
+row_length += 1 if array.length % 3 != 0
+horizontal_array = array.sort.each_slice(row_length).to_a
 
-number_of_blank = row_length - array.length % 3
+number_of_blank = row_length * 3 - array.length
 number_of_blank.times do
   horizontal_array[-1] << nil
 end
 
 vertical_array = horizontal_array.transpose
 
-t = Table.new
-t.print_table(vertical_array)
+print_table(vertical_array)
