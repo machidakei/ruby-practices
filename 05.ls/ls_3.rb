1ea390f642f9e30cd2dfa33af8a325574aa5e95f
machidakei <mochiduki.mochizuki@gmail.com>
2022-01-16 16:51:47 +0900

    レビューを受け改善2/2

--- 05.ls/ls_3.rb
+++ 05.ls/ls_3.rb
@@ -15,15 +15,11 @@ def print_table(elements_of_table)
   end
 end
 
-array = Dir.glob('*')
+array = options["r"] ?  Dir.glob('*').sort.reverse : Dir.glob('*').sort
 
 row_length = array.length / 3
 row_length += 1 if array.length % 3 != 0
-if options["r"]
-  horizontal_array = array.sort.reverse.each_slice(row_length).to_a
-else
-  horizontal_array = array.sort.each_slice(row_length).to_a
-end
+horizontal_array = array.each_slice(row_length).to_a
 
 number_of_blank = row_length * 3 - array.length
 number_of_blank.times do
