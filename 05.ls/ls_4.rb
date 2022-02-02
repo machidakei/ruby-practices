bdcfd2f61109be011935a263475934d9fb711422
machidakei <mochiduki.mochizuki@gmail.com>
2022-02-02 10:42:40 +0900

    抽象化した

--- 05.ls/ls_4.rb
+++ 05.ls/ls_4.rb
@@ -7,16 +7,21 @@ require 'etc'
 @options = ARGV.getopts('l')
 
 def print_table(elements_of_table)
-  length = elements_of_table.flatten.compact.max_by(&:length).length unless @options['l']
-  elements_of_table.each do |row|
-    row.each do |item|
-      if @options['l']
+  if @options['l']
+    elements_of_table.each do |row|
+      row.each do |item|
         print "#{item.to_s.ljust(2)} "
-      else
+      end
+      puts ''
+    end
+  else
+    length = elements_of_table.flatten.compact.max_by(&:length).length
+    elements_of_table.each do |row|
+      row.each do |item|
         print item.to_s.ljust(length), "\t"
       end
+      puts ''
     end
-    puts ''
   end
 end
 
@@ -24,37 +29,65 @@ def newfile(file)
   File::Stat.new(file)
 end
 
-array = Dir.glob('*').sort
+def listup_in_detail(files)
+  files_in_array = [files]
+  files_in_array.unshift(get_filemode_and_file_access_authorization(files),
+   get_number_of_hardlink(files), get_user_name(files), get_group_name(files), get_bytesize(files), get_date_and_time(files))
+end
 
-if @options['l']
-  filetypes = array.map { |file| newfile(file).ftype }
+def get_filemodes(files)
+  filetypes = files.map { |file| newfile(file).ftype }
   filetype_list = { 'fifo' => 'p', 'characterSpecial' => 'c', 'directory' => 'd', 'blockSpecial' => 'b', 'file' => '-', 'link' => 'l', 'socket' => 's' }
   filemodes = filetypes.map { |filetype| filetype_list[filetype] }
-  permitted_attributes = array.map { |file| File::Stat.new(file).mode.to_s(8).to_i.digits.take(3).reverse }
+end
+
+def get_file_access_authorization(files)
+  permitted_attributes = files.map { |file| File::Stat.new(file).mode.to_s(8).to_i.digits.take(3).reverse }
   file_access_authorization_list = { 0 => '---', 1 => '--x', 2 => '-w-', 3 => '-wx', 4 => 'r--', 5 => 'r-x', 6 => 'rw-', 7 => 'rwx' }
   file_access_authorization = permitted_attributes.map do |permitted_attribute|
     file_access_authorization_list[permitted_attribute[0]] +
       file_access_authorization_list[permitted_attribute[1]] +
       file_access_authorization_list[permitted_attribute[2]]
   end
-  filemode_and_file_access_authorization = filemodes.map.with_index do |filemode, i|
-    filemode + file_access_authorization[i]
-  end
-  number_of_hard_link = array.map { |file| newfile(file).nlink.to_s }
-  user_name = array.map { |file| Etc.getpwuid(newfile(file).uid).name }
-  group_name = array.map { |file| Etc.getgrgid(newfile(file).gid).name }
-  date_and_time = array.map { |file| File.open(file, 'r').mtime.strftime('%_m %e %H:%M') }
-  bytesize = array.map { |file| file.bytesize.to_s }
-  array2 = []
-  array2 << array
-  array2.unshift(filemode_and_file_access_authorization, number_of_hard_link, user_name, group_name, bytesize, date_and_time)
-  print_table(array2.transpose)
-else
-  row_length = array.length / 3
-  row_length += 1 if array.length % 3 != 0
-  horizontal_array = array.each_slice(row_length).to_a
+end
 
-  number_of_blank = row_length * 3 - array.length
+def get_filemode_and_file_access_authorization(files)
+  filemode_and_file_access_authorization = get_filemodes(files).map.with_index do |filemode, i|
+    filemode + get_file_access_authorization(files)[i]
+  end
+end
+
+def get_number_of_hardlink(files)
+  number_of_hardlink = files.map { |file| newfile(file).nlink.to_s }
+end
+
+def get_user_name(files)
+  user_name = files.map { |file| Etc.getpwuid(newfile(file).uid).name }
+end
+
+def get_group_name(files)
+  group_name = files.map { |file| Etc.getgrgid(newfile(file).gid).name }
+end
+
+def get_date_and_time(files)
+  date_and_time = files.map { |file| File.open(file, 'r').mtime.strftime('%_m %e %H:%M') }
+end
+
+def get_bytesize(files)
+  bytesize = files.map { |file| file.bytesize.to_s }
+end
+
+files = Dir.glob('*').sort
+
+if @options['l']
+  files_in_array = listup_in_detail(files)
+  print_table(files_in_array.transpose)
+else
+  row_length = files.length / 3
+  row_length += 1 if files.length % 3 != 0
+  horizontal_array = files.each_slice(row_length).to_a
+
+  number_of_blank = row_length * 3 - files.length
   number_of_blank.times do
     horizontal_array[-1] << nil
   end
