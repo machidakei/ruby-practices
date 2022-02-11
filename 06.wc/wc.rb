a30cb1ae6b236705500db8cff288d1ca95b1752b
machidakei <mochiduki.mochizuki@gmail.com>
2022-02-11 14:55:06 +0900

    lオプションの分岐を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -27,14 +27,14 @@ ARGV.each_with_index do |argument, i|
   total_lines += count_lines(text)
   total_words += count_words(text)
   total_bytesize += count_bytesize(text)
-  if @option
+  if @option['l']
     puts "\t#{count_lines(text)} #{ARGV[i]}"
   else
     puts "\t#{count_lines(text)}\t#{count_words(text)}\t#{count_bytesize(text)} #{ARGV[i]}"
   end
 end
 
-if @option
+if @option['l']
   puts "\t#{total_lines} total" if ARGV.size > 1
 elsif ARGV.size > 1
   puts "\t#{total_lines}\t#{total_words}\t#{total_bytesize} total"
